d29e0c3ab16945e4589b4ae41f25bca7539253e0
realbro <realbro.noh@gmail.com>
2023-12-30 13:45:08 +0900

    push notifications using firbase cloud messages and functions

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,13 +1,39 @@
 import 'package:chat_app/widgets/chat_messages.dart';
 import 'package:chat_app/widgets/new_message.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
 
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
   void _logout() async {
     await FirebaseAuth.instance.signOut();
+  }
+
+  void setupPushNotifications() async {
+    final fcm = FirebaseMessaging.instance;
+
+    // 1. subscribe to topic
+    fcm.subscribeToTopic('chat');
+
+    // 2. indiviual target message
+    // await fcm.requestPermission();
+    // final token = await fcm.getToken();
+    // print(
+    //     token); // you could send this token to a backend via HTTP or Firestore SDK
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    setupPushNotifications();
   }
 
   @override
